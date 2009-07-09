60a054f9bd35a92d9a5d5c7bfaa03a2fe587d6ad
shinpei <shinpei@ynu.ac.jp>
2009-07-10 05:31:41 +0900

    hi

--- gen_lsmbody.py
+++ gen_lsmbody.py
@@ -1,13 +1,27 @@
-infile = "security.c"
+import re
+
+infile = "security_body.c"
 inf = open(infile, 'r')
 L = inf.readlines()
 inf.close()
 
+funcname = ''
 for s in xrange(len(L)):
-    if ("security_ops->" in L[s]):
-        print L[s].replace(r"security_ops->", "sc_check_").strip()
-    else:
+
+    funcform = re.compile('static inline \w+ security_([a-zA-Z_]+)')
+    m = funcform.match(L[s])
+    if m != None:
+#        print L[s].strip()
+        funcname = m.group(1)
+        #print funcname
         if ("security_" in L[s]):
-            print L[s].replace("security_", "lsm_").strip()
+            print L[s].replace("security_", "sc_").strip()
         else:
-            print L[s].strip()
+                print L[s].strip()
+
+    else :
+        # there is only one function, "return 1"
+        if "return 0" in L[s]:
+            print L[s].replace("0" , "sc_%s()" % funcname)
+        elif "{" in L[s] and "}" in L[s+1]:
+            print L[s].replace("}", "return sc_%s()
