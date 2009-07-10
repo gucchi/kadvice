5e313338013a68ebea73ab744f0f5fec96bc0e3b
shinpei <shinpei@ynu.ac.jp>
2009-07-11 02:55:00 +0900

    hi

--- gen_from_secops.py
+++ gen_from_secops.py
@@ -113,7 +113,7 @@ for s in xrange(len(L)):
             outline = outline + ", %s, %s" % (argtypes[n], argvalues[n])
             outline2 = outline2 + ",%s %s" % (argtypes[n], argvalues[n])
             outline5 = outline5 + ",%s %s" % (argtypes[n], argvalues[n])
-        outline5 = outline5 + ")\n{\treturn sc_check_%s(" % funcname
+        outline5 = outline5 + ")\n{\n\treturn sc_check_%s(" % funcname
         for n in xrange(len(argtypes)):
             outline5 = outline5 + ", %s" % argvalues[n]
         outline = outline + ");"
@@ -122,7 +122,7 @@ for s in xrange(len(L)):
         outline5 = outline5.replace("(,", "(")
         outline5 = outline5 + ");\n}"
         
-        print outline5
+        print '"' + funcname + '",'
         
         counter = counter + 1
         
